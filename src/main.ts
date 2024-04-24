f836ae58a26172b33d9d4c7322c75ac0f875b017
treiiis <1841507121@qq.com>
2024-04-24 11:01:20 +0800

    安装ElementPlus

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import './style.css'
 import App from './App.vue'
 import router from "./router"
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+createApp(App).use(router).use(createPinia()).use(ElementPlus).mount('#app')
 
 
