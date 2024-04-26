97132cd67d34733eade6f84b7d99e4247b6a121b
Treiiis <1841507121@qq.com>
2024-04-26 17:16:55 +0800

    使用pinia完成优化1

---
+++ src/stores/todo.ts
@@ -0,0 +1,58 @@
+import {defineStore} from "pinia";
+import {computed, ComputedRef, onMounted, ref, watch} from "vue";
+import {Todo} from "../type/type";
+
+export const todoStore = defineStore("todo", () => {
+//默认不隐藏
+    const hideCompleted = ref(false)
+
+    const todos = ref<Todo[]>([])
+
+
+// 从缓存载入数据
+    onMounted(() => {
+        todos.value = JSON.parse(localStorage.getItem("todos") || "[]")
+    })
+
+// 保存数据方法
+    function saveData() {
+        localStorage.setItem("todos", JSON.stringify(todos.value))
+    }
+
+// 更新数据
+    watch(todos, saveData, {deep: true})//深度监听
+
+
+// 隐藏已完成
+    const filteredTodos: ComputedRef<Todo[]> = computed(() =>
+        hideCompleted.value ?
+            todos.value.filter((t) => !t.done) :
+            todos.value
+    )
+
+// 删除todo
+    function deleteTodo(id: number) {
+        todos.value = todos.value.filter((item) => item.id !== id)
+    }
+
+// 清除已完成
+    function clearAllDone() {
+        todos.value = todos.value.filter((t) => !t.done)
+    }
+
+// 响应添加todo
+    function handleAddTodo(title: string) {
+        if (!title.trim()) {
+            return alert("输入不能为空")
+        }
+        todos.value.unshift({
+            id: Date.now(),
+            title: title,
+            done: false
+        })
+    }
+
+
+    return {todos, hideCompleted, filteredTodos, deleteTodo, clearAllDone, handleAddTodo}
+
+})
