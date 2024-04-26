fd53de571e24d64f6d11fac8d128b83fc9601471
Treiiis <1841507121@qq.com>
2024-04-26 21:18:56 +0800

    1

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,15 +2,16 @@
 
 import Item from "./Item.vue";
 import { todoStore } from "../stores/todo.ts";
+import { storeToRefs } from "pinia";
 
 
-const store = todoStore()
+const { filteredTodos } = storeToRefs(todoStore())
 
 </script>
 
 <template>
   <ul class="todo-list">
-    <Item v-for="todo in store.filteredTodos" :key="todo.id" :todo="todo"></Item>
+    <Item v-for="todo in filteredTodos" :key="todo.id" :todo="todo"></Item>
   </ul>
 </template>
 
